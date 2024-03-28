177ef0667969505151f6a93219f7453c6b49c509
jenkins <saran@gmail.com>
2024-03-28 10:40:31 +0530

    Updating the code

--- tf/ec2/user-data.sh
+++ tf/ec2/user-data.sh
@@ -16,8 +16,8 @@ cd flask-test-app-for-aws-services/app/
 
 # Set up virtual environment
 sudo apt install -y python3-venv
-python3 -m venv venv
-source venv/bin/activate
+# python3 -m venv venv
+# source venv/bin/activate
 
 # Install dependencies
 pip install -r requirements.txt
