3e1239406a10fde8562885416a334fd64bfff181
jenkins <saran@gmail.com>
2024-03-28 18:54:29 +0530

    Updated app.py

--- app/app.py
+++ app/app.py
@@ -19,7 +19,7 @@ connection = pymysql.connect(**db_config)
 def index():
     # Fetch data from RDS
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM film"
+        sql = "SELECT * FROM store"
         cursor.execute(sql)
         data = cursor.fetchall()
     return render_template('index.html', data=data)
